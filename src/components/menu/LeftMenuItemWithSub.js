6b97612fbd102a27edc7ee002d906fb4a96b9034
ShiftUnion <nick.cahill@gmail.com>
2016-04-03 10:19:34 +1200

    Added some visibility wizardry

--- src/components/menu/LeftMenuItemWithSub.js
+++ src/components/menu/LeftMenuItemWithSub.js
@@ -4,14 +4,21 @@ import LeftSubMenuItem from './LeftSubMenuItem';
 
 
 export default class LeftMenuItemWithSub extends React.Component {
-  /*  constructor(props) {
-   super(props);
-   }*/
+  constructor(props) {
+    super(props);
+    this.state = {showSubMenu: false};
+    this.onClick = this.onClick.bind(this)
+  }
+
+onClick() {
+    this.setState({showSubMenu: !this.state.showSubMenu});
+
+  }
 
   render() {
     return (
       <li>
-        <Link to={'/#'}>
+        <Link to={'/calendar'} onClick={this.onClick}>
           <span className="icon color5">
             <i className="fa fa-home"/>
           </span>{this.props.displayText}
@@ -20,17 +27,21 @@ export default class LeftMenuItemWithSub extends React.Component {
             ? <span className="label label-default">{this.props.count}</span>
             : null}
         </Link>
-        <LeftSubMenuItem parentRoute={this.props.route} menuItems={this.props.subMenuItems.menuitem}/>
+        {this.state.showSubMenu ?
+          <LeftSubMenuItem parentRoute={this.props.route} menuItems={this.props.subMenuItems.menuitem}/> : null}
       </li>
     );
   }
+
+
 }
 
 // Some validation of properties
 LeftMenuItemWithSub.propTypes = {
   displayText: React.PropTypes.string.isRequired,
   count: React.PropTypes.number.isRequired,
-  subMenuItems: React.PropTypes.object
+  subMenuItems: React.PropTypes.object,
+  route: React.PropTypes.string
 };
 
 
