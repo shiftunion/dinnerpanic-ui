9c8707a821f8fb0f33c9f9e4541b6cd0f2c16f60
ShiftUnion <nick.cahill@gmail.com>
2016-04-11 22:00:33 +1200

    Wow - some quotes from famous libraries

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import configureStore from './store/configureStore';
-import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import {Route, Router, browserHistory} from 'react-router';
-import App from './components/App';
-import Plans from './components/Plans.js';
-import Meals from './containers/Meals.js';
-import Charts from './components/Charts.js';
-import Calendar from './components/Calendar.js';
+import React from "react";
+import {render} from "react-dom";
+import {Provider} from "react-redux";
+import configureStore from "./store/configureStore";
+import "./styles/styles.scss"; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import {Route, Router, browserHistory} from "react-router";
+import App from "./components/App";
+import Plans from "./components/Plans.js";
+import Meals from "./containers/Meals.js";
+import Charts from "./components/Charts.js";
+import Calendar from "./components/Calendar.js";
 
 
 const store = configureStore();
